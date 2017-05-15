a6ea58fb2968fa70b4e7378b4e14ec1cf58f237e
Leigh Dodds <leigh@ldodds.com>
2017-05-15 11:20:47 +0100

    Added FIXME

--- deploy.sh
+++ deploy.sh
@@ -81,6 +81,7 @@ git commit -m "Deploy to GitHub Pages - Static"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+# FIXME should be authorised via key
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 cd ..
